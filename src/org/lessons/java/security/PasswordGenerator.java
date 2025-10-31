11ad032f30ed9344e10e4f1c72b64b86b569bd92
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-10-31 12:32:00 +0100

    first-commit

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,10 @@
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+  }
+}
