5e37f54ab48d8c6d449bc1bfed1a6566825fe362
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-10-31 12:50:18 +0100

    feat:

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -6,5 +6,14 @@ public class PasswordGenerator {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     
+    String name = "Alberto";
+    String surname = "Sveglia";
+    int favColor = 010101;
+    byte dayOfBirth = 15;
+    byte monthOfBirth = 8;
+    short yearOfBirth = 1996;
+    char separator = '-';
+
+    
   }
 }
